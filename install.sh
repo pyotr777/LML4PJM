28644eae1452bbf63056435dc22f679e186774df
Peter Bryzgalov <peterbryz@yahoo.com>
2016-02-02 14:51:09 +0900

    Updated install script

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
-#!/bin/bash
+#!/bin/bash -s
 
 cp mypjstat ../
 cp runlml ../
 mkdir ../rms/PJM
 cp rms/PJM/* ../rms/PJM/
 cp samples/* ../samples/
+echo -e "If you see no errors then PJM support for LML DA driver was installed.\nYou can now open Eclipse PTP and start Monitor connection to this machine.\n"
+
